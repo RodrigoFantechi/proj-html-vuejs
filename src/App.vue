5bad4b619477898ac690ff219934c93f4b81ff28
RodrigoFantechi <rodrifante595@libero.it>
2022-11-21 11:13:05 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script >
+import {store} from './store.js' 
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default{
+  name: 'App',
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+
+  mounted() {
+    
+  },
+}
+</script>
+
+<template>
+  <AppHeader/>
+  <AppMain />
+  <AppFooter/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
